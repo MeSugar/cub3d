6a1e34c231b0528fe18ef2b5726acdc055d0ed15
student <student@10.0.2.15>
2021-03-01 01:15:19 +0300

    1

--- Config/color_treat.c
+++ Config/color_treat.c
@@ -1,14 +1,15 @@
 #include "cub3d.h"
 static int pass(int *i, char *line)
 {
-    if (line[*i] && (line[*i] != ' ' || line[*i] != ','))
-        return (0);
-    whitespace_skip(&i, line);
-    if (line[*i] == ',')
-        (*i)++;
+    if (line[*i] && (line[*i] == ' ' || line[*i] == ','))
+    {
+        whitespace_skip(i, line);
+        if (line[*i] == ',')
+            (*i)++;
+    }
     else
         return (0);
-    whitespace_skip(&i, line);
+    whitespace_skip(i, line);
     return (1);
 }
 
@@ -43,8 +44,13 @@ static int  color_atoi(char *line, int *i)
     int rtn;
 
     rtn = 0;
-     while (ft_isdigit(line[*i]++))
+    pass(i, line);
+    while (ft_isdigit(line[*i]))
+    {
         rtn = (rtn * 10) + (line[*i] - '0');
+        (*i)++;
+    }
+    printf("%d\n", rtn);
     if (rtn > 255 || rtn < 0)
     {
         put_error_msg("Error: Invalid color string\n");
@@ -53,17 +59,20 @@ static int  color_atoi(char *line, int *i)
     return (rtn);
 }
 
-static void convert_color(t_win *window_config, char c, int r, int g, int b)
+static int convert_color(t_win *window_config, char c, int r, int g, int b)
 {
     int color;
 
 	color = r;
 	color = (color << 8) + g;
 	color = (color << 8) + b;
-    if (c == 'C')
+    if (c == 'C' && !window_config->ceiling_color)
 		window_config->ceiling_color = color;
-	else
+	else if (c == 'F' && !window_config->floor_color)
         window_config->floor_color = color;
+    else
+        return(put_error_msg("Error: Color specified twice\n"));
+    return (1);
 }
 
 int color_treat(t_win *window_config, char *line, int i)
@@ -79,10 +88,9 @@ int color_treat(t_win *window_config, char *line, int i)
     if (line[i] && (line[i] == 'C' || line[i] == 'F'))
         i++;
     whitespace_skip(&i, line);
-    if (!color_fomat_check(line, i))
+    if (!color_format_check(line, i))
         return (0);
     if ((r = color_atoi(line, &i)) == -1 || (g = color_atoi(line, &i)) == -1 || (b = color_atoi(line, &i)) == -1)
         return (0);
-    convert_color(window_config, c, r, g, b);
-	return (1);
+    return (convert_color(window_config, c, r, g, b));
 }
