a0c3850d33cb4ab85ea1dab477d9774e38436806
Eugene Vlasov <vlasov3454@yandex.ru>
2021-03-03 18:10:16 +0300

    1

--- Draw/draw_image.c
+++ Draw/draw_image.c
@@ -8,6 +8,41 @@ static void pixel_put(t_image *image, int x, int y, int color)
     *(unsigned int*)dst = color;
 }
 
+static void draw_map(t_win *window_config)
+{
+    int x;
+    int y;
+    int w;
+    int h;
+    char **tab;
+
+    x = 0;
+    y = 0;
+    w = 50;
+    h = 50;
+    tab = window_config->map->map;
+    while (tab[x])
+    {
+        while (tab[x][y])
+        {
+            if (tab[x][y] == '1')
+            {
+                while (w < w + 10)
+                {
+                    while (h < h + 10)
+                    {
+                        pixel_put(window_config->image, w, h, 0x00FF0000);
+                        h++;
+                    }
+                    w++;
+                }
+            }
+            y++;
+        }
+        x++;
+    }
+}
+
 int draw_image(t_win *window_config)
 {
     if (!(window_config->image = calloc(1, sizeof(t_image))))
@@ -17,7 +52,7 @@ int draw_image(t_win *window_config)
         return (0);
     window_config->image->addr = mlx_get_data_addr(window_config->image->img_ptr,
     &window_config->image->bpp, &window_config->image->line_length, &window_config->image->endian);
-    pixel_put(window_config->image, 250, 250, 0x00FF0000);
+    draw_map(window_config);
     mlx_put_image_to_window(window_config->mlx_ptr, window_config->win_ptr, window_config->image->img_ptr, 0, 0);
     mlx_loop(window_config->mlx_ptr);
     return (1);
