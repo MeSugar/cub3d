2841f12872aef2151b334886cd6a01c65fe501f3
student <student@10.0.2.15>
2021-03-13 23:21:51 +0300

    1

--- Keys/turns.c
+++ Keys/turns.c
@@ -12,7 +12,7 @@ void turn_left(t_win *window_config)
 void turn_right(t_win *window_config)
 {
     window_config->player->pa += 0.1;
-    if (window_config->player->pa < 2 * PI)
+    if (window_config->player->pa > 2 * PI)
         window_config->player->pa -= 2 * PI;
     window_config->player->pdx = cos(window_config->player->pa) * 5;
     window_config->player->pdy = sin(window_config->player->pa) * 5;
