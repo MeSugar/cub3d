12f69a2657834b345dcccc1d6def519917bce2e5
Eugene Vlasov <vlasov3454@yandex.ru>
2021-03-17 01:23:24 +0300

    1

--- Buttons/movements.c
+++ Buttons/movements.c
@@ -2,32 +2,32 @@
 
 void move_forward(t_win *window_config)
 {
-    if (window_config->map->map[(int)window_config->player->py][(int)(window_config->player->px + window_config->player->pdx / 5)] == '0')
-        window_config->player->px += window_config->player->pdx / 5;
-    if (window_config->map->map[(int)(window_config->player->py + window_config->player->pdy / 5)][(int)window_config->player->px] == '0')
-    window_config->player->py += window_config->player->pdy / 5; 
+    if (window_config->map->map[(int)window_config->player->py][(int)(window_config->player->px + window_config->player->pdx * SPEED)] == '0')
+		window_config->player->px += window_config->player->pdx * SPEED;
+	if (window_config->map->map[(int)(window_config->player->py + window_config->player->pdy * SPEED)][(int)window_config->player->px] == '0')
+		window_config->player->py += window_config->player->pdy * SPEED;
 }
 
 void move_backward(t_win *window_config)
 {
-    if (window_config->map->map[(int)window_config->player->py][(int)(window_config->player->px - window_config->player->pdx / 5)] == '0')
-        window_config->player->px -= window_config->player->pdx / 5;
-    if (window_config->map->map[(int)(window_config->player->py - window_config->player->pdy / 5)][(int)window_config->player->px] == '0')
-    window_config->player->py -= window_config->player->pdy / 5; 
+    if (window_config->map->map[(int)window_config->player->py][(int)(window_config->player->px - window_config->player->pdx * SPEED)] == '0')
+		window_config->player->px -= window_config->player->pdx * SPEED;
+	if (window_config->map->map[(int)(window_config->player->py - window_config->player->pdy * SPEED)][(int)window_config->player->px] == '0')
+		window_config->player->py -= window_config->player->pdy * SPEED;
 }
 
 void move_left(t_win *window_config)
 {
-    if (window_config->map->map[(int)window_config->player->py][(int)(window_config->player->px + window_config->player->pdy / 5)] == '0')
-        window_config->player->px += window_config->player->pdy / 5;
-    if (window_config->map->map[(int)(window_config->player->py - window_config->player->pdx / 5)][(int)window_config->player->px] == '0')
-    window_config->player->py -= window_config->player->pdx / 5;
+    if (window_config->map->map[(int)window_config->player->py][(int)(window_config->player->px - window_config->player->planex * SPEED)] == '0')
+		window_config->player->px -= window_config->player->planex * SPEED;
+	if (window_config->map->map[(int)(window_config->player->py - window_config->player->planey * SPEED)][(int)window_config->player->px] == '0')
+		window_config->player->py -= window_config->player->planey * SPEED;
 }
 
 void move_right(t_win *window_config)
 {
-    if (window_config->map->map[(int)window_config->player->py][(int)(window_config->player->px - window_config->player->pdy / 5)] == '0')
-        window_config->player->px -= window_config->player->pdy / 5;
-    if (window_config->map->map[(int)(window_config->player->py + window_config->player->pdx / 5)][(int)window_config->player->px] == '0')
-        window_config->player->py += window_config->player->pdx / 5;
+    if (window_config->map->map[(int)window_config->player->py][(int)(window_config->player->px + window_config->player->planex * SPEED)] == '0')
+		window_config->player->px += window_config->player->planex * SPEED;
+	if (window_config->map->map[(int)(window_config->player->py + window_config->player->planey * SPEED)][(int)window_config->player->px] == '0')
+		window_config->player->py += window_config->player->planey * SPEED;
 }
