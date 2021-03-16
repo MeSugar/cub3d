ad7ce8a952b05f8caa6dd639f751c9405ac517c4
Eugene Vlasov <vlasov3454@yandex.ru>
2021-03-16 21:28:52 +0300

    1

--- Drawing/ray_values.c
+++ Drawing/ray_values.c
@@ -43,13 +43,20 @@ void find_wall(t_win *window_config)
         {
             window_config->ray->side_distx += window_config->ray->delta_distx;
             window_config->ray->mapx += window_config->ray->stepx;
-            window_config->ray->side = 0;
+            if (window_config->ray->stepx == 1)
+				window_config->ray->side = 0;
+			else if (window_config->ray->stepx == -1)
+				window_config->ray->side = 1;	
         }
         else
         {
             window_config->ray->side_disty += window_config->ray->delta_disty;
             window_config->ray->mapy += window_config->ray->stepy;
             window_config->ray->side = 1;
+			if (window_config->ray->stepy == 1)
+				window_config->ray->side = 2;
+			else if (window_config->ray->stepy == -1)
+				window_config->ray->side = 3;
         }
         if (window_config->map->map[window_config->ray->mapy][window_config->ray->mapx] == '1') 
             window_config->ray->hit = 1;
@@ -58,7 +65,7 @@ void find_wall(t_win *window_config)
 
 void calculate_wall_dist_n_height(t_win *window_config)
 {
-	if (window_config->ray->side == 0)
+	if (window_config->ray->side == 0 || window_config->ray->side == 1)
 		window_config->ray->wall_dist = (window_config->ray->mapx - window_config->player->px + (1 - window_config->ray->stepx) / 2) / window_config->ray->rdx;
 	else
 		window_config->ray->wall_dist = (window_config->ray->mapy - window_config->player->py + (1 - window_config->ray->stepy) / 2) / window_config->ray->rdy;
